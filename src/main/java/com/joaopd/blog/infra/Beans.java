d4a98d0d8adf41ab03bbdefe6d79aab151d3d678
João Pedro <martins.joaopedro@estudante.ufjf.br>
2024-05-19 15:13:35 -0300

    feat: added infra layer

---
+++ src/main/java/com/joaopd/blog/infra/Beans.java
@@ -0,0 +1,17 @@
+package com.joaopd.blog.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.joaopd.blog.application.core.service.PostService;
+import com.joaopd.blog.application.ports.in.PostServicePort;
+import com.joaopd.blog.application.ports.out.PostRepositoryPort;
+
+@Configuration
+public class Beans {
+    
+    @Bean
+    PostServicePort postService(PostRepositoryPort postRepositoryPort) {
+        return new PostService(postRepositoryPort);
+    }
+}
