03f5ee84fe5382a805d6eeb7d8ed27877fc503a5
João Pedro <martins.joaopedro@estudante.ufjf.br>
2024-05-19 15:10:45 -0300

    feat: created PostDTO

---
+++ src/main/java/com/joaopd/blog/application/core/dto/PostDTO.java
@@ -0,0 +1,15 @@
+package com.joaopd.blog.application.core.dto;
+
+public class PostDTO {
+
+    private String authorId;
+    private String text;
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
